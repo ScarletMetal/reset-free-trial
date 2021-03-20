71964c93a1cbf2c2a2e726b46994a340ded3a7d4
ScarletMetal <simon.kharmatsky@gmail.com>
2021-03-20 13:12:02 +0200

    Fixed reset.sh

--- reset.sh
+++ reset.sh
@@ -3,8 +3,7 @@
 JETBRAINS_CONFIG_PATH="$HOME/.config/JetBrains"
 
 # Resets evalution perioud for all versions of a given product
-function purge_evaluation {
-    echo "debug $1"
+function reset_evaluation {
     for directory in $(find $JETBRAINS_CONFIG_PATH -name "$1*" -type d); do
         echo "removing $directory/options/other.xml"
         rm -rf "$directory/options/other.xml"
@@ -17,7 +16,7 @@ function purge_evaluation {
 
 for product in IntelliJIdea PyCharm WebStorm CLion GoLand PhpStorm RubyMine Rider; do
     echo "Purgin evalution for $product"
-    purge_evaluation $product
+    reset_evaluation $product
 done
 
 rm -rf "$HOME/.java/.userPrefs"
